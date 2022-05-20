d205f404561c87059646fa26cd23e5b23d704d17
985611106@qq.com <chenfei1@e-lead.cn>
2022-05-20 16:55:56 +0800

    add pug

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const fs = require('fs');
 const debug = require('debug')('easy-bundel-kit');
 const cli = require('cli');
 const npmCli = require('./src/npm-cli');
-
 const {
     getCommandMap,
     entryName,
     shortNames,
     tips,
 } = require('./src/common');
-
 const {
     WEBPACK_DEPENDENCE,
     WEBPACK_COMMON_PLUGINS,
+    WEBPACK_PUG,
     WEBPACK_ES,
     WEBPACK_TS,
     wsBuildConfig,
 } = require('./src/build-webpack');
-
 const error = debug.extend('error');
 const rs = link => path.resolve(__dirname, link);
 
@@ -48,23 +46,31 @@ cli.main(async function () {
     debug(`检查入口文件: ${entryPath}`);
     debug(`确认导出路径: ${outputPath}`);
 
-    if (!directory || directory.length <= 0) return error(tips.error_entry_not_found);
+    if (!directory || directory.length <= 0) {
+        return error(tips.error_entry_not_found);
+    }
 
     const findEntryFile = directory.filter(filename => filename.indexOf(entryName) >= 0);
 
     debug(`发现入口文件：${findEntryFile.length > 0 ? findEntryFile.join('、') : 'Not Found'}`);
 
-    if (findEntryFile.length <= 0) return error(tips.error_entry_not_found);
+    if (findEntryFile.length <= 0) {
+        return error(tips.error_entry_not_found);
+    }
 
-    // WEBPACK - 单入口打包
+    /* webpack */
     if (options.webpack) {
+        //  - single entry bundle
         const config = wsBuildConfig(options, { entryPath, outputPath }, commandMap);
 
-        if (!config) return error(tips.error_choose_your_bundle_kit);
+        if (!config) {
+            return error(tips.error_choose_your_bundle_kit);
+        }
     
-        fs.createWriteStream(path.resolve(`${outputPath}/webpack.config.js`)).write(config);
+        fs.createWriteStream(path.resolve(`${outputPath}/webpack.config.js`))
+            .write(config);
 
-        // 初始化开发环境
+        // init develop env
         await npmCli.init({ cwd: entryPath });
 
         await npmCli.install(Array.from(new Set([
@@ -73,6 +79,7 @@ cli.main(async function () {
             { cwd: entryPath }, 'dev');
 
         await npmCli.install(Array.from(new Set([]
+            .concat(commandMap.language.includes('pug') ? WEBPACK_PUG.dependence : [])
             .concat(commandMap.language.includes('ecmascript') ? WEBPACK_ES.dependence : [])
             .concat(commandMap.language.includes('typescript') ? WEBPACK_TS.dependence : []))),
             { cwd: entryPath }, 'dev');
--- src/build-webpack.js
+++ src/build-webpack.js
@@ -16,6 +16,17 @@ const WEBPACK_COMMON_PLUGINS = [
     },
 ];
 
+const WEBPACK_PUG_CONF = () => ({
+    dependence: ['pug-loader'],
+    config: {
+        test: /\.m?pug$/,
+        exclude: DEF_EXCLUDE_PATH,
+        use: [
+            { loader: 'pug-loader' }
+        ]
+    },
+});
+
 const WEBPACK_BABEL_CONF = (dependence = [], presets = [], plugins = []) => ({
     dependence: ['babel-loader', ...dependence],
     config: {
@@ -48,7 +59,7 @@ const WEBPACK_ES = WEBPACK_BABEL_CONF(
     ],
 );
 
- // typescript
+// typescript
 const WEBPACK_TS = WEBPACK_BABEL_CONF(
     // dependence
     ['@babel/preset-typescript'],
@@ -56,11 +67,13 @@ const WEBPACK_TS = WEBPACK_BABEL_CONF(
     ['@babel/preset-typescript'],
 );
 
+// typescript
+const WEBPACK_PUG = WEBPACK_PUG_CONF();
+
 WEBPACK_DEFAULT_CONFIG.mode = 'development';
 WEBPACK_DEFAULT_CONFIG.entry = '';
 WEBPACK_DEFAULT_CONFIG.output = {};
 WEBPACK_DEFAULT_CONFIG.plugins = '${plugins}';
-
 WEBPACK_COMMON_PLUGINS.map(module => {
     const splitGroup = module.name.split('-');
     const variableName = splitGroup.map(n => `${n[0].toUpperCase()}${n.slice(1, n.length)}`).join('');
@@ -82,6 +95,7 @@ module.exports = {
     WEBPACK_CODE_PLUGINS_DESTRUCT_IMPORTS,
     WEBPACK_CODE_PLUGINS_SETTING,
     WEBPACK_COMMON_PLUGINS,
+    WEBPACK_PUG,
     WEBPACK_ES,
     WEBPACK_TS,
     wsBuildConfig(options, { entryPath, outputPath }, commandMap) {
@@ -100,6 +114,10 @@ module.exports = {
         if (options.language) {
             WEBPACK_DEFAULT_CONFIG.module = {};
             WEBPACK_DEFAULT_CONFIG.module.rules = [];
+        }
+
+        if (commandMap.language.includes('pug')) {
+            WEBPACK_DEFAULT_CONFIG.module.rules.push(WEBPACK_PUG.config);
         }
     
         if (commandMap.language.includes('ecmascript'))  {
